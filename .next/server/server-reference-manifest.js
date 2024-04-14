7392a8d5a0599ce0547abdca2b8554f0d46889f7
RevitalizeRides <RevitalizeRides@gmail.com>
2024-04-14 11:22:47 -0400

    toot

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cljZOhiDCZv1dkWAmkdUbAHP+aU1pYHypihddP45PC0=\"\n}"
---
+++ app/page.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import logo from "../public/img/workation-logo.svg"
+import workation from "../public/img/workation.jpg"
+
+export default function Page() {
+  return (
+    <div className="bg-gray-100 flex">
+      <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:max-w-full lg:w-1/2 lg:py-24 lg:px-12">
+        <div className="xl:max-w-lg xl:ml-auto">
+          <Image
+            className="h-10"
+            src={logo}
+            alt="Workation logo"
+          />
+          <Image
+            className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:object-cover sm:object-center lg:hidden"
+            src={workation}
+            alt="Woman workationing on the beach"
+          />
+          <h1 className="mt-6 text-2xl font-bold text-gray-900 leading-tight sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+            You can work from anywhere.
+            <br className="hidden lg:inline" />
+            <span className="text-indigo-500"> Take advantage of it.</span>
+          </h1>
+          <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+            Workation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're not on vacation.
+          </p>
+          <div className="mt-4 sm:mt-6">
+            <a href="#" className="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-sm text-white uppercase tracking-wider font-semibold sm:text-base">Book your escape</a>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block lg:w-1/2 lg:relative">
+        <Image
+          className="absolute inset-0 h-full w-full object-cover object-center"
+          src={workation}
+          alt="Woman workationing on the beach"
+        />
+      </div>
+    </div>
+  );
+};
